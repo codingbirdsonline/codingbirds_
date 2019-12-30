61f0913beb9c73b51ad67297426ac4147ec77113
Porus <cs.porus@gmail.com>
2019-12-30 13:10:57 -0800

    integrate-data-table

--- google-re-captcha-in-php/verify-captcha.php
+++ google-re-captcha-in-php/verify-captcha.php
@@ -21,7 +21,7 @@
                     <?php
 
                     if(isset($_POST['submitBtn'])) {
-                        $secret = '6Lc0UMoUAAAAAL56GsUUSxJnEhqp1LYByZCZSgjO';
+                        $secret = 'your_secret_key';
                         $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
                         $responseData = json_decode($verifyResponse);
 
