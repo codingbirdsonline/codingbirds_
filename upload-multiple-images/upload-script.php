448a14fbef0ac2728058a25b6358aa44428e844c
ashish29041993 <74138354+ashish29041993@users.noreply.github.com>
2020-11-08 19:04:33 +0530

    Update upload-script.php

--- upload-multiple-images/upload-script.php
+++ upload-multiple-images/upload-script.php
@@ -9,7 +9,7 @@ if (isset($_POST['uploadImageBtn'])) {
         $result = move_uploaded_file($imageTmpName,$uploadFolder.$imageName);
 
         // save to database
-        $query = "INSERT INTO bird_multiple_images SET imgName='$imageName' " ;
+        $query = "INSERT INTO bird_multiple_images  (imgName) VALUES ('$imageName')" ;
         $run = $connection->query($query) or die("Error in saving image".$connection->error);
     }
     if ($result) {
