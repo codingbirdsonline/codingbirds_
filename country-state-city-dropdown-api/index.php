050aa34df36176661c2712ba0004dfab1f35ea36
Ankit <codingbirdsonline@gmail.com>
2023-05-27 17:33:20 +0530

    CORS error prevented

--- country-state-city-dropdown-api/index.php
+++ country-state-city-dropdown-api/index.php
@@ -2,6 +2,10 @@
 require_once "Dropdown.php";
 require_once "Helper.php";
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
 /**
  * Validate & check endpoint exists
  * otherwise reject the request
